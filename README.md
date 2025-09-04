2afcee84d123402a8c12fccafe9acac76661263a
Antonio <antoniozafra1997@gmail.com>
2025-09-04 08:47:06 +0200

    Reestructrurando FrontEnd y Backend

--- README.md
+++
@@ -1,6 +0,0 @@
-# Welcome to my Portfolio
-
-This is a file README.md
-
-Click here to visit online -->[Link Here](https://antoniozafra.github.io/portfolio)
-
--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/portfolio/', // <- nombre exacto de tu repo
-  plugins: [react()],
-});
